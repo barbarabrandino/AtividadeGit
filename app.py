c1e529df1b37059a242252178c9c5f0f028dcd00
BARBARA HELENA PRETO BRANDINO <1051392421024@fatec.corp>
2026-02-26 09:58:42 -0300

    fix: corrige erro de sintaxe na rota status

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return {"mensagem": "API funcionando corretamente"}
+
+
 @app.route("/status")
 def status():
-    return {"status": "API online"}
+    return {"status": API online}
 
 if __name__ == "__main__":
     app.run(debug=True)
