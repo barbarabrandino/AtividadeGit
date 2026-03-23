d67f7221cdd346a668e122ecaf675d981173783f
BARBARA HELENA PRETO BRANDINO <1051392421024@fatec.corp>
2026-03-23 08:14:59 -0300

    melhora na rota de gerenciamento de biblioteca

--- src/app.py
+++ src/app.py
@@ -2,14 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return {"mensagem": "API funcionando corretamente"}
+    return "Sistema de Gerenciamento de Biblioteca"
 
-
-@app.route("/status")
-def status():
-    return {"status": API online}
+@app.route("/sobre")
+def sobre():
+    return "Projeto desenvolvido na disciplina de Integração e Entrega Contínua"
 
 if __name__ == "__main__":
     app.run(debug=True)
